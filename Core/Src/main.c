092218444ca10d40fac4a667ca46bb77f149a1d9
YEK-Kayra <124110070+YEK-Kayra@users.noreply.github.com>
2024-08-05 17:50:31 +0300

    Check

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -35,6 +35,7 @@
   *		"The __CLOSED expression can be written at the end of the header guard.
   *		 If you remove this part, the related code will work and complied
   *
+  *
   ******************************************************************************
   */
 
